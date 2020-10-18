7868698f97096a2eaaa18395ccd77477697fb4b9
Kato Shinya <yourdream28@gmail.com>
2020-10-18 22:40:35 +0900

    add

---
+++ src/main/java/org/thinkit/generator/catalog/engine/factory/CatalogConstructorProcess.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2020 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.thinkit.generator.catalog.engine.factory;
+
+import org.thinkit.generator.common.factory.resource.ConstructorProcess;
+
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import lombok.ToString;
+
+/**
+ * カタログクラスのコンストラクタの処理を生成する処理を定義したファクトリークラスです。
+ * <p>
+ * {@link #createResource()} メソッドを使用することでコンストラクタの処理を表現する文字列を取得することができます。
+ *
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ */
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public final class CatalogConstructorProcess extends ConstructorProcess {
+
+    /**
+     * 引数として渡された情報を基に {@link CatalogConstructorProcess} クラスの新しいインスタンスを生成し返却します。
+     *
+     * @param variableName 変数名
+     * @exception NullPointerException 引数として {@code null} が渡された場合
+     */
+    private CatalogConstructorProcess(@NonNull String variableName) {
+        super(variableName);
+    }
+
+    /**
+     * 引数として渡された情報を基に {@link CatalogConstructorProcess} クラスの新しいインスタンスを生成し返却します。
+     *
+     * @param variableName 変数名
+     * @param getterName   ゲッター名
+     * @exception NullPointerException 引数として {@code null} が渡された場合
+     */
+    private CatalogConstructorProcess(@NonNull String variableName, @NonNull String getterName) {
+        super(variableName, getterName);
+    }
+
+    public static ConstructorProcess of(@NonNull String variableName) {
+        return new CatalogConstructorProcess(variableName);
+    }
+
+    public static ConstructorProcess of(@NonNull String variableName, @NonNull String getterName) {
+        return new CatalogConstructorProcess(variableName, getterName);
+    }
+
+    @Override
+    public String createResource() {
+        return null;
+    }
+}
