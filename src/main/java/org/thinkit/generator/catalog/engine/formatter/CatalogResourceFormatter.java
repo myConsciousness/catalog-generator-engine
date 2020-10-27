641d77e66cb6192627ef1ac097906c01c5743c0f
Kato Shinya <yourdream28@gmail.com>
2020-10-27 15:12:33 +0900

    update documentation

--- src/main/java/org/thinkit/generator/catalog/engine/formatter/CatalogResourceFormatter.java
+++ src/main/java/org/thinkit/generator/catalog/engine/formatter/CatalogResourceFormatter.java
@@ -22,12 +22,52 @@ import lombok.EqualsAndHashCode;
 import lombok.NonNull;
 import lombok.ToString;
 
+/**
+ * {@link CatalogMatrix} クラスに格納されたリソース情報を基にカタログリソースを生成する処理を定義したフォーマッタークラスです。
+ * <p>
+ * {@link #newInstance()} メソッドを使用することで {@link CatalogResourceFormatter}
+ * クラスの新しいインスタンスを生成することができます。 {@link CatalogResourceFormatter}
+ * クラスの新しいインスタンスを生成した後は {@link #format(CatalogMatrix)}
+ * メソッドを呼び出し整形処理を行ってください。整形処理が正常終了した場合は生成されたリソースが格納された
+ * {@link CatalogResourceGroup} が返却されます。
+ *
+ * <pre>
+ * 操作例:
+ * <code>
+ * CatalogResourceFormatter.newInstance().format(catalogMatrix).foreach(catalogResource -> {
+ *      // do something like
+ *      catalogResource.getPackageName();
+ *      catalogResource.getClassName();
+ *      catalogResource.getResource();
+ * });
+ * </code>
+ * </pre>
+ *
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ */
 @ToString
 @EqualsAndHashCode
 public final class CatalogResourceFormatter implements ResourceFormatter<CatalogMatrix, CatalogResourceGroup> {
 
+    /**
+     * デフォルトコンストラクタ
+     */
+    private CatalogResourceFormatter() {
+    }
+
+    /**
+     * {@link CatalogResourceFormatter} クラスの新しいインスタンスを生成し返却します。
+     *
+     * @return {@link CatalogResourceFormatter} クラスの新しいインスタンス
+     */
+    public static ResourceFormatter<CatalogMatrix, CatalogResourceGroup> newInstance() {
+        return new CatalogResourceFormatter();
+    }
+
     @Override
-    public CatalogResourceGroup format(@NonNull CatalogMatrix arg0) {
-        return null;
+    public CatalogResourceGroup format(@NonNull CatalogMatrix catalogMatrix) {
+        return CatalogResourceGroup.of();
     }
 }
