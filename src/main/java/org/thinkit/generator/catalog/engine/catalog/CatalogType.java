081288d75132ba6a215f755a59e7921bb7e12753
Kato Shinya <yourdream28@gmail.com>
2020-10-27 16:35:58 +0900

    add

---
+++ src/main/java/org/thinkit/generator/catalog/engine/catalog/CatalogType.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2020 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.thinkit.generator.catalog.engine.catalog;
+
+import org.thinkit.api.catalog.BiCatalog;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * カタログ種別を管理するカタログです。
+ *
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ */
+@RequiredArgsConstructor
+public enum CatalogType implements BiCatalog<CatalogType, String> {
+
+    /**
+     * カタログ
+     */
+    CATALOG(0, "Catalog"),
+
+    /**
+     * バイカタログ
+     */
+    BI_CATALOG(1, "BiCatalog");
+
+    /**
+     * コード値
+     */
+    @Getter
+    private final int code;
+
+    /**
+     * タグ
+     */
+    @Getter
+    private final String tag;
+}
