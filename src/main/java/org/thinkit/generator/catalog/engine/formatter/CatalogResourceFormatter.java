3d79bd798b7d8d47cb8daf6803eb55a84466f9ff
Kato Shinya <yourdream28@gmail.com>
2020-10-27 14:54:37 +0900

    add

---
+++ src/main/java/org/thinkit/generator/catalog/engine/formatter/CatalogResourceFormatter.java
@@ -0,0 +1,17 @@
+package org.thinkit.generator.catalog.engine.formatter;
+
+import org.thinkit.generator.catalog.engine.dto.CatalogMatrix;
+import org.thinkit.generator.catalog.engine.dto.CatalogResourceGroup;
+import org.thinkit.generator.common.formatter.ResourceFormatter;
+
+import lombok.NonNull;
+
+public final class CatalogResourceFormatter implements ResourceFormatter<CatalogMatrix, CatalogResourceGroup> {
+
+    @Override
+    public CatalogResourceGroup format(@NonNull CatalogMatrix arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
